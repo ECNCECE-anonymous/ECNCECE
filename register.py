550b1ab5a3c89f6607927cd4b46cb396ae5eec1f
ECNCECE-anonymous <ECNCECE-anonymous@proton.me>
2025-05-13 13:40:50 +0200

    Update register.py

--- register.py
+++ register.py
@@ -1,21 +1,6 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="EnvJax-v5",  # Unique ID for your environment
-    entry_point="env_jax_v5:MyEnv",  # Path to the environment class
-)
-
-register(
-    id="Env-v4",  # Unique ID for your environment
-    entry_point="env_v4:MyEnv",  # Path to the environment class
-)
-
-register(
-    id="Env-energy-v1",  # Unique ID for your environment
-    entry_point="env_energy:MyEnv",  # Path to the environment class
-)
-
-register(
     id="Env-energy-v2",  # Unique ID for your environment
-    entry_point="env_energy_v2:MyEnv",  # Path to the environment class
+    entry_point="env:MyEnv",  # Path to the environment class
 )
