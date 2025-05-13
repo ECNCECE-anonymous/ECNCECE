b982d11e20c6816704ffcabc0d592ea97bc1d748
ECNCECE-anonymous <ECNCECE-anonymous@proton.me>
2025-05-13 13:40:24 +0200

    Update and rename parallel_v2.py to parallel.py

--- parallel.py
+++ parallel.py
@@ -2,7 +2,7 @@ from multiprocessing import Pool
 
 
 class ParallelEvaluator(object):
-    def __init__(self, num_workers, eval_function, timeout=None, maxtasksperchild=None):
+    def __init__(self, num_workers, eval_function, timeout=None, maxtasksperchild=None, n_seasons=4, seed=1361, energy_costs=False):
         """
         eval_function should take one argument, a tuple of (genome object, config object),
         and return a single float (the genome's fitness).
@@ -12,19 +12,13 @@ class ParallelEvaluator(object):
         self.num_workers = num_workers
         self.maxtasksperchild = maxtasksperchild
         self.pool = Pool(processes=num_workers, maxtasksperchild=maxtasksperchild)
-
-    '''
-    def __del__(self):
-        self.pool.close()
-        self.pool.join()
-        self.pool.terminate()
-    '''
+        self.n_seasons = n_seasons
+        self.seed = seed
+        self.energy_costs = energy_costs
 
     def __del__(self):
         """Clean up the current pool."""
         if self.pool:
-            #print("Closing pool...")
-            #self.pool.close()
             print("Terminating pool...")
             self.pool.terminate()
             try:
@@ -37,8 +31,6 @@ class ParallelEvaluator(object):
     def reinitialize(self):
         """Clean up the current pool."""
         if self.pool:
-            #print("Closing pool...")
-            #self.pool.close()
             print("Terminating pool...")
             self.pool.terminate()
             try:
@@ -52,7 +44,7 @@ class ParallelEvaluator(object):
     def evaluate_(self, genomes, config):
         jobs = []
         for ignored_genome_id, genome in genomes:
-            jobs.append(self.pool.apply_async(self.eval_function, (genome, config)))
+            jobs.append(self.pool.apply_async(self.eval_function, (genome, config, True, 100_000, True, self.n_seasons, self.seed, self.energy_costs)))
 
         timeout_occurred = False
         # assign the fitness back to each genome
@@ -62,7 +54,6 @@ class ParallelEvaluator(object):
                     genome.fitness = job.get(timeout=3)
                 else:
                     genome.fitness = job.get(timeout=self.timeout)
-            #except:
             except Exception as e:
                 print("Timeout occured for job: ", e)
                 genome.fitness = None
@@ -71,11 +62,8 @@ class ParallelEvaluator(object):
 
     def evaluate(self, genomes, config):
         self.evaluate_(genomes, config)
-
         retry = False
-        #print()
         for genome_id, genome in genomes:
-            #print("genome (", genome_id, ") fitness: ", genome.fitness)
             if genome.fitness is None:
                 retry = True
                 break
@@ -85,9 +73,6 @@ class ParallelEvaluator(object):
             self.reinitialize()  # Clean up and recreate the pool
             print("reinitialized")
             self.evaluate_(genomes, config)
-            #print()
-            #for genome_id, genome in genomes:
-                #print("genome (", genome_id, ") fitness: ", genome.fitness)
 
 
         
